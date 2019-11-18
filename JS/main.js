5b8f673b98b5c3a3a897102ce70a2b9b3d34395d
Tabitha <bidee.tabitha@gmail.com>
2019-11-18 14:32:12 +0100

    fetch choice

--- JS/main.js
+++ JS/main.js
@@ -1,31 +1,13 @@
-// Regular map
-function regular_map() {
-    var var_location = new google.maps.LatLng(40.725118, -73.997699);
+document
+  .querySelector(".fstdropdown-select")
+  .addEventListener("click", async () => {
+    console.log("test");
 
-    var var_mapoptions = {
-        center: var_location,
-        zoom: 14
-    };
+    const response = await fetch(
+      "api.openweathermap.org/data/2.5/forecast?q=Ghent,be"
+    );
+    const city = await response.json();
+    console.log(city);
+  });
 
-    var var_map = new google.maps.Map(document.getElementById("map-container"),
-        var_mapoptions);
-
-    var var_marker = new google.maps.Marker({
-        position: var_location,
-        map: var_map,
-        title: "New York"
-    });
-}
-
-// Initialize maps
-google.maps.event.addDomListener(window, 'load', regular_map);
-
-// Carousel options
-
-$('.carousel').carousel({
-    interval: 3000,
-})
-
-// Material Select
-$('.mdb-select').materialSelect({
-});
+console.log(document.querySelector(".fstselected"));
