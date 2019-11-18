82db29d8754806bf3035c45255f625e7a583f1e4
Stijn <stijn.peeters.it@gmail.com>
2019-11-18 15:03:21 +0100

    delete complicated dropdown

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,14 @@
-document
-  .querySelector(".fstdropdown-select")
-  .addEventListener("click", async () => {
-    console.log("test");
+// document
+//   .querySelector(".fstdropdown-select")
+//   .addEventListener("click", async () => {
+//     console.log("test");
 
-    const response = await fetch(
-      "api.openweathermap.org/data/2.5/forecast?q=Ghent,be"
-    );
-    const city = await response.json();
-    console.log(city);
-  });
-
-console.log(document.querySelector(".fstselected"));
+//     const response = await fetch(
+//       "api.openweathermap.org/data/2.5/forecast?q=Ghent,be"
+//     );
+//     const city = await response.json();
+//     console.log(city);
+//   });
+document.querySelector(".fstselected").addEventListener("click", () => {
+  console.log(document.querySelector(".fstdropdown fstlist"));
+});
