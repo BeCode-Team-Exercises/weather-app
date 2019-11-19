0af2de9d095a6854bdb2ae7316eecbf3e1c3a5fe
Stijn <stijn.peeters.it@gmail.com>
2019-11-19 18:51:46 +0100

    submit button to show

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,14 @@
 document.getElementById("button").addEventListener("click", async () => {
   const selectCity = document.querySelector(".form-control");
   let chosenCity = selectCity.value;
-
   const response = await fetch(
     `http://api.openweathermap.org/data/2.5/forecast?q=${chosenCity}&units=metric&appid=2118afe2ed56240f86bbd108daffe6b8`
   );
   const jsonCity = await response.json();
-
   const list = jsonCity.list;
   var listArray = [];
-
   const listm = list.forEach(datum => {
     const weatherInfo = datum.weather[0];
-
     if (datum.dt_txt.includes("15:00")) {
       var event = new Date(datum.dt_txt);
       var options = {
@@ -21,13 +17,11 @@ document.getElementById("button").addEventListener("click", async () => {
         month: "long",
         day: "numeric"
       };
-
       listArray.push(
         event.toLocaleDateString("en-EN", options),
         datum.main.temp,
         weatherInfo.main
       );
-
       let j = 0;
       for (var i = 1; i <= 5; i++) {
         let day = `.day${i}`;
